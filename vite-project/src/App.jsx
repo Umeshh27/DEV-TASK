569b91006f5199e3d93df85e34ec27df67ba0fac
Umeshh27 <23MH1A05E7@acoe.edu.in>
2025-09-09 17:09:34 +0530

    feat: add Customers route and link in App component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,19 @@
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Register from './pages/Register';
-import Login from './pages/Login';
+
+import Customers from './pages/Customers';
 
 function App() {
 	return (
 		<Router>
 			<nav className="nav-bar">
+				<Link to="/customers">Customers</Link>
 				<Link to="/register">Register</Link>
 				<Link to="/login">Login</Link>
 			</nav>
 			<Routes>
+				<Route path="/customers" element={<Customers />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
 			</Routes>

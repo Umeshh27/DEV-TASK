ddb2591da7c8fc1808dcbed763a966d2866b205c
Umeshh27 <23MH1A05E7@acoe.edu.in>
2025-09-10 12:09:13 +0530

    fix: correct import order for CSS styles in App component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
